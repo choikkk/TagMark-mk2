69001fe0895246f43da9660aadd56aed9928d5eb
LeeKyuMoon <lkmoon1734@gmail.com>
2023-10-28 14:22:05 +0900

    1028lee

--- js/newModal.js
+++ js/newModal.js
@@ -28,6 +28,43 @@ saveBtn.onclick= function() {
 	console.log("URL:", urlInputValue);
 	console.log("태그:", tagTextareaValue);
 
+	// HTML 요소 생성
+	let additionalBoxDiv = document.createElement("div");
+	additionalBoxDiv.classList.add("additional-box");
+
+	// 제목 상자
+	let h2Element = document.createElement("h2");
+	h2Element.textContent = titleInputValue;
+	additionalBoxDiv.appendChild(h2Element);
+
+	// URL 상자
+	let pElement = document.createElement("p");
+	pElement.textContent = urlInputValue;
+	additionalBoxDiv.appendChild(pElement);
+
+	// 태그 상자
+	let additionalBoxTagDiv = document.createElement("div");
+	additionalBoxTagDiv.classList.add("additional-box-tag");
+	additionalBoxDiv.appendChild(additionalBoxTagDiv);
+
+
+	// 태그 받고 생성해주는 for문
+	for (let i = 0; i < tagTextareaValue.length; i++) {
+		let yellowCircleDiv = document.createElement("div");
+		yellowCircleDiv.classList.add("yellow-circle");
+		additionalBoxTagDiv.appendChild(yellowCircleDiv);
+	  
+		let pElement = document.createElement("p");
+		pElement.textContent = tagTextareaValue[i];
+		additionalBoxTagDiv.appendChild(pElement);
+	}
+
+	
+	// 요소를 추가할 위치를 찾아서 추가
+	let SecondBoxDiv = document.getElementsByClassName("second-box")[0];
+	SecondBoxDiv.appendChild(additionalBoxDiv);
+
+
 	newModal.style.display = "none";
 
 	titleInput.value = ""; // 제목 입력 필드 초기화
