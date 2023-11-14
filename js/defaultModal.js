332ee0f309b7e0db57c6df05fba3deba6a4724ad
choikkk <sixballoons@naver.com>
2023-11-14 17:43:51 +0900

    1114 18choi

--- js/defaultModal.js
+++ js/defaultModal.js
@@ -7,19 +7,28 @@ var modal = document.getElementById('newModal');
 
 // 모달 열기 이벤트
 openModalBtn.onclick = function() {
-    modal.style.display = 'block';
+  modal.classList.remove('closing'); // 새로운 클래스 추가
+  modal.style.display = 'block';
 }
 
 // 모달 닫기 이벤트
 closeModalBtn.onclick = function() {
-    modal.style.display = 'none';
+  modal.classList.add('closing'); // 새로운 클래스 추가
+  setTimeout(function() {
+      modal.style.display = 'none';
+      modal.classList.remove('closing'); // 새로운 클래스 제거
+  }, 500); // 애니메이션 지속 시간과 동일한 시간을 설정
 }
 
 // 모달 외부 클릭 시 닫기
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = 'none';
-    }
+  if (event.target == modal) {
+      modal.classList.add('closing'); // 새로운 클래스 추가
+      setTimeout(function() {
+          modal.style.display = 'none';
+          modal.classList.remove('closing'); // 새로운 클래스 제거
+      }, 500); // 애니메이션 지속 시간과 동일한 시간을 설정
+  }
 }
 
 /*카테고리 작은 모달 수정할거
