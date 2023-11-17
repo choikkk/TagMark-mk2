139a05968bee07ed9b8d457e1626c776df728ff0
bugrice <manssik0820@gmail.com>
2023-11-17 15:56:00 +0900

    111716sik

--- js/defaultModal.js
+++ js/defaultModal.js
@@ -1,13 +1,13 @@
 // 모달 열기 버튼과 모달 닫기 버튼 참조
-var openModalBtn = document.getElementById('openModalBtn');
-var closeModalBtn = document.getElementById('closeModalBtn');
+const openModalBtn = document.getElementById('openModalBtn');
+const closeModalBtn = document.getElementById('closeModalBtn');
 
 // 모달 참조
-var modal = document.getElementById('newModal');
+const modal = document.getElementById('newModal');
+const categoryModal = document.getElementById('newCategoryModal');
 
 // 모달 열기 이벤트
 openModalBtn.onclick = function() {
-  modal.classList.remove('closing'); // 새로운 클래스 추가
   modal.style.display = 'block';
 }
 
@@ -31,26 +31,28 @@ window.onclick = function(event) {
   }
 }
 
-/*카테고리 작은 모달 수정할거
-const openModalBtn = document.getElementById('openModalBtn');
-const modal = document.getElementById('myModal');
-const closeModalBtn = document.querySelector('.close');
+// 카테고리 추가 모달
+const openCategoryModalBtn = document.getElementById('openCategoryModalBtn');
+const closeCategoryModalBtn = document.getElementById('closeCategoryModalBtn');
 
-function openModal() {
-  modal.style.display = 'block';
+openCategoryModalBtn.onclick = function() {
+  categoryModal.style.display = 'block';
 }
 
-function closeModal() {
-  modal.style.display = 'none';
+closeCategoryModalBtn.onclick = function() {
+  categoryModal.classList.add('closing'); // 새로운 클래스 추가
+  setTimeout(function() {
+    categoryModal.style.display = 'none';
+    categoryModal.classList.remove('closing'); // 새로운 클래스 제거
+  }, 500); // 애니메이션 지속 시간과 동일한 시간을 설정
 }
-
-openModalBtn.addEventListener('click', openModal);
-closeModalBtn.addEventListener('click', closeModal);
 
 window.addEventListener('click', function (event) {
-  if (event.target === modal) {
-    closeModal();
-  }
+  if (event.target == categoryModal) {
+    categoryModal.classList.add('closing'); // 새로운 클래스 추가
+    setTimeout(function() {
+      categoryModal.style.display = 'none';
+      categoryModal.classList.remove('closing'); // 새로운 클래스 제거
+    }, 500); // 애니메이션 지속 시간과 동일한 시간을 설정
+}
 });
-
-*/
