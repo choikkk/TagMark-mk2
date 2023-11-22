365a2303e2782f936a12c5d2ac5b9a956d3ce10b
bugrice <manssik0820@gmail.com>
2023-11-22 17:50:31 +0900

    112217sik

--- js/Button.js
+++ js/Button.js
@@ -12,11 +12,10 @@ const OldButton = document.getElementById("oldBtn"); //오래된 정렬 버튼
 const UnClassifiedSidebarArea = document.getElementById("UnclassifiedUL"); // 사이드바 UnClassified 공간
 
 var AllRecnetlyTag = []; // 북마크 한개의 최근 추가 태그 리스트 배열
-
 var AllTagList = []; // 전체 태그 배열
 var UnClassifiedTagList = []; // UnClassified 사이드바 태그 삽입
-var AllBookMarkList = []; // 전체 북마크 배열
 var SetTagList = []; // 최근 추가 태그 관련 배열
+var AllBookMarkList = []; // 전체 북마크 배열
 
 // 정렬에 필요한 변수
 let SortBookMarkList = []; // 유니코드 순서 정렬된 북마크 배열
@@ -192,7 +191,6 @@ saveBtn.onclick= function() {
 		console.log('삭제 버튼 클릭>>> ' + AllBookMarkList);
 	});
 	
-	
 	// 태그 리스트 업데이트
 	function updateTagLists(deletedTags) {
 		// 삭제된 태그를 모든 태그 목록과 현재 태그 목록에서 찾아 제거
@@ -224,35 +222,6 @@ saveBtn.onclick= function() {
 	
 		// 현재 추가 태그 목록
 		let CurrentTagList = SetTagList; // 현재 추가 태그 배열
-	
-		// 최근 추가 태그 공간 태그 추가
-		// for (let i = 0; i < CurrentTagList.length; i++) {
-		// 	let CurAdditionalBoxTagDiv = document.createElement("div");
-		// 	CurAdditionalBoxTagDiv.classList.add("additional-box-tag");
-		// 	currentTagArea.appendChild(CurAdditionalBoxTagDiv);
-	
-		// 	let yellowCircleDiv = document.createElement("div");
-		// 	yellowCircleDiv.classList.add("yellow-circle");
-		// 	CurAdditionalBoxTagDiv.appendChild(yellowCircleDiv);
-	
-		// 	let pElement = document.createElement("p");
-		// 	pElement.textContent = CurrentTagList[i];
-		// 	CurAdditionalBoxTagDiv.appendChild(pElement);
-		// }
-		// 최근 추가 태그 공간 태그 추가
-		// for (let i = 0; i < RecnetlyTag.length; i++) {
-		// let CurAdditionalBoxTagDiv = document.createElement("div");
-		// CurAdditionalBoxTagDiv.classList.add("additional-box-tag");
-		// currentTagArea.appendChild(CurAdditionalBoxTagDiv);
-
-		// let yellowCircleDiv = document.createElement("div");
-		// yellowCircleDiv.classList.add("yellow-circle");
-		// CurAdditionalBoxTagDiv.appendChild(yellowCircleDiv);
-
-		// let pElement = document.createElement("p");
-		// pElement.textContent = RecnetlyTag[i];
-		// CurAdditionalBoxTagDiv.appendChild(pElement);
-		// }
 	}
 	
 	// UnClassifiedTagList 업데이트
@@ -345,8 +314,6 @@ saveBtn.onclick= function() {
 		AllAdditionalBoxTagDiv.appendChild(pElement);
 	}
 
-
-	
 	// 최근 추가 태그 목록
 	let CurrentTagList = []; // 최근 추가 태그 배열
 
@@ -373,9 +340,7 @@ saveBtn.onclick= function() {
 		CurAdditionalBoxTagDiv.appendChild(pElement);
 	}
 	
-
 	// 사이드바 UnClassified 태그 추가 부분
-
 	// UnClassified html 부분 초기화
 	UnClassifiedSidebarArea.innerHTML = "";
 
@@ -400,8 +365,6 @@ saveBtn.onclick= function() {
 		UnClassifiedSidebarArea.appendChild(UnclassifiedDiv);
 	}
 
-
-	
 	// 요소를 추가할 위치를 찾아서 추가
 	let SecondBoxDiv = document.getElementsByClassName("second-box")[0];
 	SecondBoxDiv.appendChild(additionalBoxDiv);
@@ -412,79 +375,199 @@ saveBtn.onclick= function() {
 	titleInput.value = ""; // 제목 입력 필드 초기화
 	urlInput.value = ""; // URL 입력 필드 초기화s
 	tagTextarea.value = ""; // 태그 입력 필드 초기화
-	// console.log("중복 태그 목록 >>>")
-	// console.log(AllTagList)
-	// console.log("전체 태그 목록 >>>")
-	// console.log(SetTagList)
-}
 
+//////////////////////////////////////	// 전체 정렬 버튼	//	//////////////////////////////////////////////
+	AllButton.onclick= function() {
+		// 요소를 추가할 위치를 찾아서 추가
+		let SecondBoxDiv = document.getElementsByClassName("second-box")[0];
+		// 해당 공간 html요소 초기화
+		SecondBoxDiv.innerHTML = '';
 
+		SortAll(SortBookMarkList); // 북마크 정렬 알고리즘
+		for(let i=0; i<SortBookMarkList.length; i++){
+			// 클로저를 사용하여 ElementBookMark 변수를 유지
+			let ElementBookMark = SortBookMarkList[i];
+			// 북마크 Div요소 생성
+			let additionalBoxDiv = document.createElement("div");
+			additionalBoxDiv.classList.add("additional-box");
 
-// 전체 정렬 버튼
-AllButton.onclick= function() {
-	// 요소를 추가할 위치를 찾아서 추가
-	let SecondBoxDiv = document.getElementsByClassName("second-box")[0];
-	// 해당 공간 html요소 초기화
-	SecondBoxDiv.innerHTML = '';
+			// 제목 상자
+			let h2Element = document.createElement("h2");
+			h2Element.textContent = SortBookMarkList[i][0];
+			additionalBoxDiv.appendChild(h2Element);
 
-	SortAll(SortBookMarkList); // 북마크 정렬 알고리즘
-	for(let i=0; i<SortBookMarkList.length; i++){
-		// 북마크 Div요소 생성
-		let additionalBoxDiv = document.createElement("div");
-		additionalBoxDiv.classList.add("additional-box");
+			// URL 상자
+			let aElementUrl = document.createElement("a");
+			aElementUrl.textContent = SortBookMarkList[i][1];
+			aElementUrl.href = SortBookMarkList[i][1];
+			aElementUrl.target = "_blank"; // 새창에서 링크 열기
+			additionalBoxDiv.appendChild(aElementUrl);
 
-		// 제목 상자
-		let h2Element = document.createElement("h2");
-		h2Element.textContent = SortBookMarkList[i][0];
-		additionalBoxDiv.appendChild(h2Element);
+			// 북마크쪽 아래 태그 Div
+			let BmAdditionalBoxTagDiv = document.createElement("div");
+			BmAdditionalBoxTagDiv.classList.add("additional-box-tag");
+			additionalBoxDiv.appendChild(BmAdditionalBoxTagDiv);
+			for(let j = 0; j < SortBookMarkList[i][2].length; j++){
+				let yellowCircleDiv = document.createElement("div");
+					yellowCircleDiv.classList.add("yellow-circle");
+					BmAdditionalBoxTagDiv.appendChild(yellowCircleDiv);
+				
+					let pElement = document.createElement("p");
+					pElement.textContent = SortBookMarkList[i][2][j];
+					BmAdditionalBoxTagDiv.appendChild(pElement);
+				
+			}
 
-		// URL 상자
-		let aElementUrl = document.createElement("a");
-		aElementUrl.textContent = SortBookMarkList[i][1];
-		aElementUrl.href = SortBookMarkList[i][1];
-		aElementUrl.target = "_blank"; // 새창에서 링크 열기
-		additionalBoxDiv.appendChild(aElementUrl);
-
-		// 북마크쪽 아래 태그 Div
-		let BmAdditionalBoxTagDiv = document.createElement("div");
-		BmAdditionalBoxTagDiv.classList.add("additional-box-tag");
-		additionalBoxDiv.appendChild(BmAdditionalBoxTagDiv);
-		for(let j = 0; j < SortBookMarkList[i][2].length; j++){
-			let yellowCircleDiv = document.createElement("div");
-				yellowCircleDiv.classList.add("yellow-circle");
-				BmAdditionalBoxTagDiv.appendChild(yellowCircleDiv);
-			
-				let pElement = document.createElement("p");
-				pElement.textContent = SortBookMarkList[i][2][j];
-				BmAdditionalBoxTagDiv.appendChild(pElement);
-			
-		}
-		// 북마크 박스 태그 Div 아이콘 추가
-		let SImageDiv = document.createElement("div");
-		SImageDiv.classList.add("SImages");
-		BmAdditionalBoxTagDiv.appendChild(SImageDiv);
-	
-		let BmEditIcon = document.createElement("img");
-		BmEditIcon.classList.add("BImages");
-		BmEditIcon.src = "Images/pencil.png";
-		BmEditIcon.id = "onBookModify"
-		SImageDiv.appendChild(BmEditIcon);
-	
-		let BmDeleteIcon = document.createElement("img");
-		BmDeleteIcon.classList.add("BImages");
-		BmDeleteIcon.src = "Images/trash.png";
-		BmDeleteIcon.id = "onBookDelete"
-		SImageDiv.appendChild(BmDeleteIcon);
-
-		SecondBoxDiv.appendChild(additionalBoxDiv);
+			// 북마크 박스 태그 Div 아이콘 추가
+			let SImageDiv = document.createElement("div");
+			SImageDiv.classList.add("SImages");
+			BmAdditionalBoxTagDiv.appendChild(SImageDiv);
 		
-	}
-	// console.log("전체 북마크 리스트 : " ,AllBookMarkList);
-	// console.log("정렬된 북마크 리스트 : " , SortBookMarkList);
-	// console.log("////////////////")
-	
-}
 
+			let BmEditIcon = document.createElement("img");
+			BmEditIcon.classList.add("BImages");
+			BmEditIcon.src = "Images/pencil.png";
+			BmEditIcon.id = "onBookModify"
+			SImageDiv.appendChild(BmEditIcon);
+
+			// 마우스를 올렸을 때 스타일 변경
+			BmEditIcon.addEventListener('mouseover', function() {
+				BmEditIcon.style.cursor = 'pointer'; // 마우스 커서를 포인터로 변경
+				BmEditIcon.style.opacity = '0.7';   // 원하는 스타일 변경
+			});
+
+			// 마우스가 벗어났을 때 스타일 원래대로 변경
+			BmEditIcon.addEventListener('mouseout', function() {
+				BmEditIcon.style.cursor = 'default'; // 마우스 커서를 기본값으로 변경
+				BmEditIcon.style.opacity = '1';      // 원래 스타일로 변경
+			});
+		
+	//////////////////////////정렬에서 삭제 버튼 기능 구현
+	// 북마크 삭제 버튼 기능 구현
+			let BmDeleteIcon = document.createElement("img");
+			BmDeleteIcon.classList.add("BImages");
+			BmDeleteIcon.src = "Images/trash.png";
+			BmDeleteIcon.id = "onBookDelete"
+			SImageDiv.appendChild(BmDeleteIcon);
+
+			// 마우스를 올렸을 때 스타일 변경
+			BmDeleteIcon.addEventListener('mouseover', function() {
+				BmDeleteIcon.style.cursor = 'pointer'; // 마우스 커서를 포인터로 변경
+				BmDeleteIcon.style.opacity = '0.7';   // 원하는 스타일 변경
+			});
+
+			// 마우스가 벗어났을 때 스타일 원래대로 변경
+			BmDeleteIcon.addEventListener('mouseout', function() {
+				BmDeleteIcon.style.cursor = 'default'; // 마우스 커서를 기본값으로 변경
+				BmDeleteIcon.style.opacity = '1';      // 원래 스타일로 변경
+			});
+
+			BmDeleteIcon.addEventListener('click', function() {
+				// 북마크가 AllBookMarkList 배열에서의 인덱스를 찾기
+				const index = findBookmarkIndex(ElementBookMark);
+			
+				// AllBookMarkList 배열에서 북마크 제거
+				if (index !== -1) {
+					const deletedBookmark = AllBookMarkList.splice(index, 1)[0];
+			
+					// 태그 목록 업데이트
+					updateTagLists(deletedBookmark[2]);
+			
+					// UnClassifiedTagList 업데이트
+					updateUnClassifiedTagList(deletedBookmark[2]);
+			
+					// 정렬된 배열에서 북마크 제거
+					removeFromSortedArrays(deletedBookmark);
+				}
+			
+				// 북마크를 나타내는 HTML 요소를 DOM에서 제거
+				additionalBoxDiv.remove();
+			
+				console.log('삭제 버튼 클릭>>> ' + AllBookMarkList);
+			});
+
+				// 태그 리스트 업데이트
+			function updateTagLists(deletedTags) {
+				// 삭제된 태그를 모든 태그 목록과 현재 태그 목록에서 찾아 제거
+				deletedTags.forEach(deletedTag => {
+					const allTagIndex = AllTagList.indexOf(deletedTag.toLowerCase());
+					if (allTagIndex !== -1) {
+						AllTagList.splice(allTagIndex, 1);
+					}
+				});
+			
+				// 해당 공간 html 요소 초기화
+				allTagArea.innerHTML = '';
+				// currentTagArea.innerHTML = '';
+			
+				// 태그 추가
+				for (let i = 0; i < AllTagList.length; i++) {
+					let AllAdditionalBoxTagDiv = document.createElement("div");
+					AllAdditionalBoxTagDiv.classList.add("additional-box-tag");
+					allTagArea.appendChild(AllAdditionalBoxTagDiv);
+			
+					let yellowCircleDiv = document.createElement("div");
+					yellowCircleDiv.classList.add("yellow-circle");
+					AllAdditionalBoxTagDiv.appendChild(yellowCircleDiv);
+			
+					let pElement = document.createElement("p");
+					pElement.textContent = AllTagList[i];
+					AllAdditionalBoxTagDiv.appendChild(pElement);
+				}
+			
+				// 현재 추가 태그 목록
+				let CurrentTagList = SetTagList; // 현재 추가 태그 배열
+			}
+			// UnClassifiedTagList 업데이트
+			function updateUnClassifiedTagList(deletedTags) {
+				// 삭제된 태그를 UnClassifiedTagList에서 찾아 제거
+				deletedTags.forEach(deletedTag => {
+					const unClassifiedIndex = UnClassifiedTagList.indexOf(deletedTag.toLowerCase());
+					if (unClassifiedIndex !== -1) {
+						UnClassifiedTagList.splice(unClassifiedIndex, 1);
+					}
+				});
+			
+				// UnClassifiedTagList 업데이트
+				UnClassifiedSidebarArea.innerHTML = "";
+				for (let j = 0; j < UnClassifiedTagList.length; j++) {
+					let UnclassifiedDiv = document.createElement('div');
+					let UnclassifiedSpan = document.createElement('span');
+			
+					UnclassifiedSpan.innerText = UnClassifiedTagList[j];
+			
+					UnclassifiedSpan.setAttribute('herf', '#');
+					UnclassifiedDiv.appendChild(UnclassifiedSpan);
+					UnClassifiedSidebarArea.appendChild(UnclassifiedDiv);
+				}
+			}
+			// 전체 북마크 리스트에서 북마크 인덱스 찾기
+			function findBookmarkIndex(bookmark) {
+				for (let i = 0; i < AllBookMarkList.length; i++) {
+					if (
+						AllBookMarkList[i][0] === bookmark[0] &&
+						AllBookMarkList[i][1] === bookmark[1] &&
+						JSON.stringify(AllBookMarkList[i][2]) === JSON.stringify(bookmark[2])
+					) {
+						return i;
+					}
+				}
+				return -1;
+			}
+			// 정렬된 배열에서 북마크 목록 삭제
+			function removeFromSortedArrays(bookmark) {
+				SortBookMarkList = SortBookMarkList.filter(bm => bm !== bookmark);
+				LatestSortBookMarkList = LatestSortBookMarkList.filter(bm => bm !== bookmark);
+				OldSortBookMarkList = OldSortBookMarkList.filter(bm => bm !== bookmark);
+			}
+			SecondBoxDiv.appendChild(additionalBoxDiv);
+		}
+	}
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // 최신 순서 정렬 버튼 기능
 LatestButton.onclick = function () {
 	if(filter < 2){
@@ -608,11 +691,9 @@ LatestButton.onclick = function () {
 
 			SecondBoxDiv.appendChild(additionalBoxDiv);
 	  
-		  }
+		  	}
 		}
 	}
-
-	
 }
 
 // 오래된 순서 정렬 버튼 기능
